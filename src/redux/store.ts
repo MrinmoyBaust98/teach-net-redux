9aaf5bdb7db0d8ff23378e1e035aedd2ac133fa5
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-07-09 00:51:18 +0600

    [Done] Implement Product & availablity Cheek

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
+import productReducer from './features/products/productSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    product: productReducer,
   },
 });
 
