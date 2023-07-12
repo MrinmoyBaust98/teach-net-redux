54ca3042e58d34b2e23ff14167fa4b0569e29839
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-07-13 04:06:50 +0600

    [Done] Create Async thunk complete

--- src/redux/features/user/userSlice.ts
+++ src/redux/features/user/userSlice.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { auth } from '@/lib/firebase';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
@@ -35,6 +36,23 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.user.email = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.user.email = null;
+        state.isError = true;
+        state.error = action.error.message!;
+      });
+  },
 });
 
 // export const { } = productSlice.actions;
