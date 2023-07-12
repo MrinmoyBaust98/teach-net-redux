778ba6b2480f1dd8365f30b1027973412341c7e4
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-07-13 02:00:29 +0600

    [Done] Create User Slice and Connect with Store

---
+++ src/redux/features/user/userSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+// import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface IUserState {
+  user: {
+    email: string | null;
+  };
+  isLoading: boolean;
+  isError: boolean;
+  error: string | null;
+}
+
+const initialState: IUserState = {
+  user: {
+    email: null,
+  },
+  isLoading: false,
+  isError: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+});
+
+// export const { } = productSlice.actions;
+
+export default userSlice.reducer;
